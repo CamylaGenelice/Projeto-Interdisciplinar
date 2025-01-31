7f51c084b160853a6d247a80e44e58630b35a8f2
Camyla Genelice <genelicecamyla@gmail.com>
2025-01-31 10:45:28 -0300

    update
    TryCatch

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,62 +3,91 @@ const useServices = require("../services/services")
 
 
 const create =  async (req, res) =>{
-    const {name,username,email,senha} = req.body
 
-    if (!name || !username || !email || !senha){
-        res.status(400).send({mensagem:"Preencha todos os campos para registro!"})
-    }
-    
-    const user =  await useServices.createServices(req.body)
-    // verificação
-    if(!user){
-        return res.status(400).send({message: "Error ao criar usuario! "})
-    }
-    
-    res.status(201).send({
-        message:("dados recebidos"),
-        user:{
-            id: user._id,
-            name,
-            username,
-            email,
-            
+    try {
+
+        const {name,username,email,senha} = req.body
+        
+        if (!name || !username || !email || !senha){
+            res.status(400).send({mensagem:"Preencha todos os campos para registro!"})
         }
-       
-    })
+        
+        const user =  await useServices.createServices(req.body)
+        // verificação
+        if(!user){
+            return res.status(400).send({message: "Error ao criar usuario! "})
+        }
+        
+        res.status(201).send({
+            message:("dados recebidos"),
+            user:{
+                id: user._id,
+                name,
+                username,
+                email,
+                
+            }
+           
+        })
+
+    } 
+    catch (error) {
+        res.status(500).send({msg:error.message})
+    }
+
+    
     
 }
 
 const findAll = async (req, res) => {
-    const users = await useServices.findAllServices()
+    try {
+        const users = await useServices.findAllServices()
 
-    if(users.length === 0){
-        return res.status(400).send({messagen: "Não ha usuários castrados"})
-    }
-    res.send(users)
+        if(users.length === 0){
+            return res.status(400).send({messagen: "Não ha usuários castrados"})
+        }
+        res.send(users)
 }
+     catch (error) {
+        res.status(500).send({msg:error.message})
+    }
+
+}    
 
 const findById = async (req, res) =>{
     // A requisição traz o user
-    const user = req.user
-    res.send(user)
+    try {
+        const user = req.user
+        res.send(user)
+    } 
+    catch (error) {
+        res.status(500).send({msg:error.message})
+    }
+    
 }
 
 const update = async (req,res) => {
-    const {name,username,email,senha} = req.body
+    try {
+        const {name,username,email,senha} = req.body
 
-    if (!name && !username && !email && !senha){
-        res.status(400).send({mensagem:"Submeta pelo menos um campo para atualizar!"})
-    }
+        if (!name && !username && !email && !senha){
+            res.status(400).send({mensagem:"Submeta pelo menos um campo para atualizar!"})
+        }
     
-    const {id,user} = req
+        const {id,user} = req
     
     /*const user = await useServices.findByIdService(id)*/
 
-    await useServices.updateService(
-        id, name,username,email,senha,
-    )
-    return res.send({msg: "Usuario foi atualizado com sucesso! "})
+        await useServices.updateService(
+            id, name,username,email,senha,
+        )
+        return res.send({msg: "Usuario foi atualizado com sucesso! "})
+        } 
+    catch (error) {
+        res.status(500).send({msg:error.message})
+    }
+    
+    
 }
 
 module.exports = {
