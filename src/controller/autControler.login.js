fe1b811d00bfdb9731772e62ff4aa7bb3bc03297
Camyla Genelice <genelicecamyla@gmail.com>
2025-02-01 17:35:26 -0300

    update 1.3.1

--- src/controller/autControler.login.js
+++ src/controller/autControler.login.js
@@ -1,18 +1,30 @@
 import bcrypt from 'bcrypt'
-import {loginService} from ""
+import {loginService} from "../services/aut.service.js"
 
 const login = async (req,res) => {
+    try {
+        const {email, senha} = req.body
 
-    const {email, senha} = req.body
+        const user = await loginService(email)
 
-    const user = loginService(email)
+        if(!user){
+            return res.status(404).send({msg:"senha ou usuario incorreto!"})
+        }
 
-    const senhaIsValid = bcrypt.compare(senha, user.senha)
+        const senhaIsValid = bcrypt.compareSync(senha, user.senha)
 
+        if(!senhaIsValid){
+            return res.status(404).send({msg:"senha ou usuario incorreto!"})
+        }
+
+        res.send("Login okay")
+
+    }
+     catch (error) {
+        res.status(500).send(error.message)
+    }
 
     
-    
-    
-    res.send("LOgin okay")
+
 }
 export {login}
