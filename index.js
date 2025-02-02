73f3078ab7922c179973ffe37856af8d54c49ead
Camyla Genelice <genelicecamyla@gmail.com>
2025-02-02 18:55:48 -0300

    update 1.3.2
    novas telas adicionadas

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+// ARQUIVO PRINCIPAL INDEX
+
 import express from "express"
 import connectDataBase from "./src/databse/banco.js"
 import dotenv from "dotenv"
 dotenv.config()
+import cors from "cors";
 
 import userRoute from "./src/route/route.js"
 import autRoute from "./src/route/autenticacao.route.js"
 
 
+
 const app = express()
 
 const port = process.env.PORT || 18000
+
+app.use(cors({
+    origin: "http://127.0.0.1:5500", // Permite apenas requisições desse domínio
+    methods: "GET,POST,PUT,DELETE", // Define os métodos HTTP permitidos
+    allowedHeaders: "Content-Type,Authorization" // Define os headers permitidos
+}));
+
 
 connectDataBase()
 app.use(express.json())
--- src/route/route.js
+++ src/route/route.js
@@ -4,6 +4,9 @@ import {validID,validUser} from "../middlewares/global.middleware.js"
 
 
 const route = express.Router()
+
+route.post("users/register", userController.create)
+
 //rota para criar
 route.post("/", userController.create)
 
